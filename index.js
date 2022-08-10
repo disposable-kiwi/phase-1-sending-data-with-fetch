21a13c91bdbf7016cf1d407e6c9c9a3b06e86924
Min <biffboffkev@gmail.com>
2022-08-10 16:29:47 -0400

    commit tree d3a98b2306f45148a3e92800db8449ec3a44a9d7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { bodyParser } = require("json-server");
+
 // Add your code here
+function submitData(obj){
+    return fetch("http://localhost:3000/users",{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name:"Steve",
+            email:"steve@steve.com",
+        })
+    })
+        .then(res => res.json())
+            .then((data) =>{ 
+                let child = document.createElement('h1');
+                child.textContent = `${data.id}`;
+                document.querySelector("#append-place").appendChild(child);
+            })
+                .catch((error)=>{
+                    let errorMessage = document.createElement('h1');
+                    errorMessage.textContent = `${error.message}`;
+                    document.querySelector("#append-place").appendChild(errorMessage);
+                });
+}
